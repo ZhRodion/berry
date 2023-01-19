83fc7a5c7a789b4ed8f5830ac6b1149cba506120
Rodion <103366016+ZhRodion@users.noreply.github.com>
2023-01-19 20:51:05 +0400

    Исправляет скрипт и дописывает стили

--- source/js/script.js
+++ source/js/script.js
@@ -1,20 +1,20 @@
 /* в этот файл добавляет скрипты*/
-let navMain = document.querySelector('.main-nav');
+// let navMain = document.querySelector('.main-nav');
 let navToggle = document.querySelector('.main-nav__toggle');
 let navList = document.querySelector('.nav-list')
 
-navMain.classList.remove('main-nav--nojs');
+// navMain.classList.remove('main-nav--nojs');
 
 navToggle.addEventListener('click', function () {
-if (navMain.classList.contains('main-nav--closed')) {
-  navMain.classList.remove('main-nav--closed');
-  navMain.classList.add('main-nav--opened');
-  navList.classList.remove('nav-list--closed')
+if (navList.classList.contains('nav-list--closed')) {
+  navList.classList.remove('nav-list--closed');
   navList.classList.add('nav-list--opened');
+  // navList.classList.remove('nav-list--closed')
+  // navList.classList.add('nav-list--opened');
 } else {
-navMain.classList.add('main-nav--closed');
-navMain.classList.remove('main-nav--opened');
-navList.classList.remove('nav-list--opened')
-navList.classList.add('nav-list--closed');
+  navList.classList.add('nav-list--closed');
+  navList.classList.remove('nav-list--opened');
+// navList.classList.remove('nav-list--opened')
+// navList.classList.add('nav-list--closed');
 }
 })
