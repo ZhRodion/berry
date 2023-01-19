af6d39e9687ff5e770a05c2f568172347a757287
Rodion <103366016+ZhRodion@users.noreply.github.com>
2023-01-19 17:51:43 +0400

    Написал скрипт открытия/закрытия меню

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,20 @@
 /* в этот файл добавляет скрипты*/
+let navMain = document.querySelector('.main-nav');
+let navToggle = document.querySelector('.main-nav__toggle');
+let navList = document.querySelector('.nav-list')
+
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function () {
+if (navMain.classList.contains('main-nav--closed')) {
+  navMain.classList.remove('main-nav--closed');
+  navMain.classList.add('main-nav--opened');
+  navList.classList.remove('nav-list--closed')
+  navList.classList.add('nav-list--opened');
+} else {
+navMain.classList.add('main-nav--closed');
+navMain.classList.remove('main-nav--opened');
+navList.classList.remove('nav-list--opened')
+navList.classList.add('nav-list--closed');
+}
+})
