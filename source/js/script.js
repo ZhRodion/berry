c4632c8a9d3c04ba9670ec535e5c02e5054be989
Rodion <103366016+ZhRodion@users.noreply.github.com>
2023-01-25 22:20:53 +0400

    Пофиксил меню и путь файла до скрипта

--- source/js/script.js
+++ source/js/script.js
@@ -9,12 +9,8 @@ navToggle.addEventListener('click', function () {
 if (navList.classList.contains('nav-list--closed')) {
   navList.classList.remove('nav-list--closed');
   navList.classList.add('nav-list--opened');
-  // navList.classList.remove('nav-list--closed')
-  // navList.classList.add('nav-list--opened');
 } else {
   navList.classList.add('nav-list--closed');
   navList.classList.remove('nav-list--opened');
-// navList.classList.remove('nav-list--opened')
-// navList.classList.add('nav-list--closed');
 }
 })
